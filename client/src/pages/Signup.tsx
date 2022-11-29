d5123e7e079c0084dd3bb8f040e60bdd077d437c
jake-the-cake <askjake331@gmail.com>
2022-11-29 13:46:03 -0500

    success on signup

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,11 +1,11 @@
-import React, { MouseEvent, useContext } from 'react'
+import React, { Dispatch, SetStateAction, useContext, useState } from 'react'
 import { useNavigate } from 'react-router'
 import { MainCard } from '../components/MainCard'
 import { PageContext } from '../context/UserContext'
 import { changeActiveLink } from '../functions/changeActiveLink'
-import axios from 'axios'
 
 export const Signup = () => {
+  const [ isSubmitted, setIsSubmitted ]: [ boolean, Dispatch<SetStateAction<boolean>> ] = useState( false )
   const ctx = useContext( PageContext )
   const navigate = useNavigate()
 
@@ -22,44 +22,56 @@ export const Signup = () => {
       const username = document.getElementById( 'username' ) as HTMLInputElement
       const email = document.getElementById( 'email' ) as HTMLInputElement
       const password = document.getElementById( 'password' ) as HTMLInputElement
-      try {
-        fetch( 'http://localhost:4200/users/add', {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            "username": username.value,
-            "email": email.value,
-            "password": password.value
-          })
-        }).then( res => res.json() ).then( data => data ).catch( err => console.error( err.message ))
-      }
-      catch ( err ) {
-        console.log( err.message )
-        console.log( err )
-      }
+      fetch( 'http://localhost:4200/users/add', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "username": username.value,
+          "email": email.value,
+          "password": password.value
+        })
+      })
+      .then( res => res.json() )
+      .then( data => {
+        console.log( data )
+        setIsSubmitted( true )
+      })
+      .catch( err => console.error( err.message ))
     }
 
   return (
-    <MainCard
-      title='Create An Account'
-      subtitle='Fill in the form below. All fields are required.'
-      content={
-        <form className='form__container'>
-          <label htmlFor='username'>Username</label>
-          <input id='username' placeholder='Choose a user name' type="text" />
-          <label htmlFor='email'>Email</label>
-          <input id='email' placeholder='Enter a valid email address' type="text" />
-          <label htmlFor='password'>Password</label>
-          <input id='password' placeholder='Create a password' type="text" />
-          <div className='buttons__horizontal'>
-            <button onClick={ handleRegister }>Create Account</button>
-            <button onClick={ () => navigate( '/login' )} className='button__secondary'>Login Instead</button>
-          </div>
-        </form>
+    <>
+    {
+      isSubmitted
+      ? <MainCard
+          title="Success"
+          subtitle="Your account has been added"
+          content={
+            <>Yup</>
+          }
+        />
+      : <MainCard
+          title='Create An Account'
+          subtitle='Fill in the form below. All fields are required.'
+          content={
+            <form className='form__container'>
+              <label htmlFor='username'>Username</label>
+              <input id='username' placeholder='Choose a user name' type="text" />
+              <label htmlFor='email'>Email</label>
+              <input id='email' placeholder='Enter a valid email address' type="text" />
+              <label htmlFor='password'>Password</label>
+              <input id='password' placeholder='Create a password' type="text" />
+              <div className='buttons__horizontal'>
+                <button onClick={ handleRegister }>Create Account</button>
+                <button onClick={ () => navigate( '/login' )} className='button__secondary'>Login Instead</button>
+              </div>
+            </form>
+          }
+        />
       }
-    />
+    </>
   )
 }
